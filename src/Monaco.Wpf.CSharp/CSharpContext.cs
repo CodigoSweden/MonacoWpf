b9ce73a8f4f6cb39009f36864b6dc18b130a0753
Joakim Antonsson <joakim.antonsson@live.com>
2018-04-15 12:08:02 +0200

    refactored csharpcontext to get a func and a class context

--- src/Monaco.Wpf.CSharp/CSharpContext.cs
+++ src/Monaco.Wpf.CSharp/CSharpContext.cs
@@ -16,7 +16,7 @@ namespace Monaco.Wpf.CSharp
         public string Name { get; set; }
         public string Description { get; set; }
     }
-    public class CSharpContext
+    public abstract class CSharpContext
     {
         public AdhocWorkspace Workspace { get; }
         public Project Project { get; }
@@ -27,27 +27,32 @@ namespace Monaco.Wpf.CSharp
         public List<string> Types { get; }
         string _template;
         int _starLine;
-        public Guid Id { get;  }
+        public Guid Id { get; }
+
+        protected List<string> _usings;
+        protected List<string> _types;
+        protected List<MetadataReference> _references;
+
 
         public CSharpContext(
-            List<Argument> arguments,
-            Argument returnType,
-            string initializeCode,
-            string helpers,
             List<string> usings,
             List<string> types,
             List<MetadataReference> references)
         {
+            _usings = usings;
+            _types = types;
+            _references = references;
+
             Id = Guid.NewGuid();
             Types = types;
-            (_template, _starLine) = GetTemplate(arguments, returnType, initializeCode,helpers, usings);
+            (_template, _starLine) = GetTemplate();
 
             Workspace = new AdhocWorkspace();
             Project = Workspace.AddProject("temp", LanguageNames.CSharp)
                 .AddMetadataReferences(references);
-            
+
             Document = Project.AddDocument("f", "");
-            
+
         }
 
         public int GetPosition(int line, int column)
@@ -55,7 +60,7 @@ namespace Monaco.Wpf.CSharp
             return SourceText.Lines.GetPosition(new Microsoft.CodeAnalysis.Text.LinePosition(line + _starLine - 1, column - 1));
         }
 
-        public Document WithText(string code,bool withTemplate = true)
+        public Document WithText(string code, bool withTemplate = true)
         {
             code = withTemplate ? string.Format(_template, code) : code;
             SourceText = SourceText.From(code);
@@ -64,37 +69,50 @@ namespace Monaco.Wpf.CSharp
         }
 
 
-        /// <summary></summary>
-        /// <param name="arguments"></param>
-        /// <param name="returnType"></param>
-        /// <param name="initializeCode"></param>
-        /// <param name="usings"></param>
-        /// <returns></returns>
-        public static (string template,int scriptStartLine) GetTemplate(
-            List<Argument> arguments,
+        public abstract (string template, int scriptStartLine) GetTemplate();
+    }
+
+    public class CSharpFuncContext : CSharpContext
+    {
+        protected List<Argument> _arguments;
+        protected Argument _returnType;
+        protected string _initializeCode;
+        protected string _helpers;
+
+        public CSharpFuncContext(List<Argument> arguments,
             Argument returnType,
             string initializeCode,
             string helpers,
-            List<string> usings)
+            List<string> usings,
+            List<string> types,
+            List<MetadataReference> references) : base( usings, types, references)
         {
-            
-            string[] argumentNames = arguments.Select(x => x.Name).ToArray();
+            _arguments = arguments;
+            _returnType = returnType;
+            _initializeCode = initializeCode;
+            _helpers = helpers;
+        }
 
-            var fsignatur = $"Func<{string.Join(", ",arguments.Select(x => x.Type))}{(arguments.Any() ? "," : "")}{returnType.Type}>";
-            var marguments = string.Join(", ", arguments.Select(x => $"{x.Type} {x.Name}"));
-            
+        public override (string template, int scriptStartLine) GetTemplate()
+        {
+
+            string[] argumentNames = _arguments.Select(x => x.Name).ToArray();
+
+            var fsignatur = $"Func<{string.Join(", ", _arguments.Select(x => x.Type))}{(_arguments.Any() ? "," : "")}{_returnType.Type}>";
+            var marguments = string.Join(", ", _arguments.Select(x => $"{x.Type} {x.Name}"));
+
             var code = $@"
-{string.Join("\r\n",usings.Select(x => $"using {x};"))}
+{string.Join("\r\n", _usings.Select(x => $"using {x};"))}
 
 public class DynamicScript
 {{{{
     public {fsignatur} TheScript = new {fsignatur}(Script);
     /// <summary></summary>
-    {string.Join("\r\n",arguments.Select(x=>$@"    /// <param name=""{x.Name}"">{x.Description}</param>"))}
+    {string.Join("\r\n", _arguments.Select(x => $@"    /// <param name=""{x.Name}"">{x.Description}</param>"))}
     /// <returns></returns>
-    public static {returnType.Type} Script({marguments})
+    public static {_returnType.Type} Script({marguments})
     {{{{
-        {initializeCode}
+        {_initializeCode.Replace("{", "{{").Replace("}", "}}")}
 
         // The script starts here
 {{0}}
@@ -102,12 +120,49 @@ public class DynamicScript
     }}}}
 }}}}
 
-{helpers}
+{_helpers.Replace("{","{{").Replace("}","}}")}
 ";
             var startIndex = code.IndexOf("{0}");
             var lines = code.Substring(0, startIndex).Split(new string[] { "\n" }, StringSplitOptions.None).Count() - 1;
             return (code, lines);
         }
-        
+
     }
+
+
+    public class CSharpClassContext : CSharpContext
+    {
+        string _helpers;
+        public CSharpClassContext(List<Argument> arguments,
+            string helpers,
+            List<string> usings,
+            List<string> types,
+            List<MetadataReference> references): base(usings,types,references)
+        {
+            _helpers = helpers;
+        }
+
+        public override (string template, int scriptStartLine) GetTemplate()
+        {
+
+         
+            var code = $@"
+{string.Join("\r\n", _usings.Select(x => $"using {x};"))}
+
+public class GlobalScripts
+{{{{
+    // Class Code goes here
+{{0}}
+
+{_helpers.Replace("{", "{{").Replace("}", "}}")}
+
+}}}}
+";
+            var startIndex = code.IndexOf("{0}");
+            var lines = code.Substring(0, startIndex).Split(new string[] { "\n" }, StringSplitOptions.None).Count() - 1;
+            return (code, lines);
+        }
+
+    }
+
 }
